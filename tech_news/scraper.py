ed3be020c6c3cea3ca5783b8015b4048048ddcec
Lethiciahas <lethiciahas@gmail.com>
2022-11-08 20:39:17 -0400

    feat: criado a função fetch

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,7 +1,16 @@
+import time
+import requests
 # Requisito 1
 
 def fetch(url):
-    """Seu código deve vir aqui"""
+    time.sleep(1)
+    try:
+        header = {"user-agent": "Fake user-agent" }
+        response = requests.get(url, header=header, timeout=3)
+        if response.status_code == 200:
+            return response.text
+    except requests.ReadTimeout:
+        return None
 
 
 # Requisito 2
